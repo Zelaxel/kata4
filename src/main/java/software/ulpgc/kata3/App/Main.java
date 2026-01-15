bf0914340fac57a0778e768616451df4266551a5
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2026-01-15 18:28:14 +0000

    Changed Main.

--- src/main/java/software/ulpgc/kata3/App/Main.java
+++ src/main/java/software/ulpgc/kata3/App/Main.java
@@ -1,9 +1,39 @@
 package software.ulpgc.kata3.App;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import software.ulpgc.kata3.io.RemoteMonsterLoader;
+import software.ulpgc.kata3.model.Monster;
+import software.ulpgc.kata3.tasks.HistogramBuilder;
+import software.ulpgc.kata3.viewModel.Histogram;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     static void main() {
-        new RemoteMonsterLoader().loadAll().forEach(System.out::println);
+        List<Monster> monsters = new RemoteMonsterLoader(Main::parse).loadAll();
+        Histogram<Boolean> histogram = new HistogramBuilder(monsters).build(m -> Stream.of(m.isLarge()));
+        histogram.forEach(bin -> System.out.println(bin + ": " + histogram.count(bin)));
+    }
+
+    public static Monster parse(JSONObject input) {
+        return new Monster(
+                input.getString("name"),
+                toType(input.getString("type")),
+                input.getBoolean("isLarge"),
+                toGames(input.getJSONArray("games"))
+        );
+    }
+
+    private static List<String> toGames(JSONArray jsonArray) {
+        List<JSONObject> jsonObjects = new ArrayList<>();
+        jsonArray.forEach(j -> jsonObjects.add((JSONObject) j));
+        return jsonObjects.stream().map(j -> j.getString("game")).toList();
+    }
+
+    private static Monster.Type toType(String type) {
+        return Monster.Type.valueOf(type.replace(" ", "_").replace("???", "unknown"));
     }
 }
