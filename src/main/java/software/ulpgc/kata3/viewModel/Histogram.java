799c3b75e6f7d7a704275c160d7507c916aca6bc
Zelaxel <alejandroramirezlorenzo2003@gmail.com>
2026-01-15 18:27:48 +0000

    Added Histogram class.

---
+++ src/main/java/software/ulpgc/kata3/viewModel/Histogram.java
@@ -0,0 +1,26 @@
+package software.ulpgc.kata3.viewModel;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Histogram<T> implements Iterable<T> {
+    private final Map<T, Integer> map;
+
+    public Histogram() {
+        map = new HashMap<>();
+    }
+    
+    public int count(T bin){
+        return map.getOrDefault(bin, 0);
+    }
+    
+    public void put(T bin){
+        map.put(bin, count(bin) + 1);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return map.keySet().iterator();
+    }
+}
